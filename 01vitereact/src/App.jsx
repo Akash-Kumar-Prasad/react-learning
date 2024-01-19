8b8f6ad45c7dae4ae5e6e0859df6a16d19db35d7
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-01-19 14:05:19 +0530

    create app via react and vite, export, name of function and file should start with capital letter and file extension should be used accordingly

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+  return (
+    <>
+    <Chai />
+    <h3>chai garma garam</h3>
+    <p>testing if tea is good or not</p>
+    </>
+   
+   
+  )
+}
+
+export default App
